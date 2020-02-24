63d6b534ec5d2f6a175d7714db3642d6acfb13b9
Aziz Apriyanto <aziz.apriyanto@bukalapak.com>
2020-02-24 19:13:07 +0700

    completing scenarios and refactor

--- src/test/java/stepsDefinition/Calculator.java
+++ src/test/java/stepsDefinition/Calculator.java
@@ -1,6 +1,7 @@
 package stepsDefinition;
 
 import cucumber.api.DataTable;
+import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -40,8 +41,8 @@ public class Calculator {
         wait.until(webDriver -> ((JavascriptExecutor) driver).executeScript("return document.readyState").toString().equals("complete"));
     }
 
-    @When("^I enter following values and press enter button$")
-    public void iEnterFollowingValuesAndPressEnterButton(DataTable args) {
+    @When("^I enter following values for \"([^\"]*)\" operation$")
+    public void iEnterFollowingValues(String operation, DataTable args) {
         iframe = driver.findElement(By.id("fullframe"));
         driver.switchTo().frame(iframe);
 
@@ -54,44 +55,74 @@ public class Calculator {
 
         Action enterValue1 = builder.sendKeys(canvas, v1.get("value1")).build();
         enterValue1.perform();
-        Action operator = builder.sendKeys(canvas, op.get("operator")).build();
-        operator.perform();
-        Action enterValue2 = builder.sendKeys(canvas, v2.get("value2")).build();
-        enterValue2.perform();
-        Action pressEnter = builder.sendKeys(Keys.ENTER).build();
-        pressEnter.perform();
+
+        if (operation.equals("basic")) {
+            Action operator = builder.sendKeys(canvas, op.get("operator")).build();
+            operator.perform();
+            Action enterValue2 = builder.sendKeys(canvas, v2.get("value2")).build();
+            enterValue2.perform();
+        } else {
+            Action btn = builder.sendKeys(canvas, op.get("button")).build();
+            btn.perform();
+        }
     }
 
-    @Then("^I should be able to see$")
+    @Then("^I should be able to see the right result$")
     public void iShouldBeAbleToSee(DataTable result) throws IOException {
         Map<String, String> expect = result.asMap(String.class, String.class);
         String expected = expect.get("expected");
+        takeScreenShot();
+        assertEquals(expected,recognizeImage());
+    }
 
+    @And("^I press \"([^\"]*)\" button$")
+    public void iPressButton(String button) {
+        Actions builder = new Actions(driver);
+        switch (button) {
+            case "ENTER":
+                builder.sendKeys(canvas, Keys.ENTER).build().perform();
+                break;
+            case "CE":
+                builder.sendKeys(canvas, "C").build().perform();
+                break;
+            default:
+                break;
+        }
+    }
+
+    @And("^user close the browser$")
+    public void userCloseTheBrowser() {
+        driver.close();
+    }
+
+    @Then("^The result should reset$")
+    public void theResultShouldReset() throws IOException {
+        takeScreenShot();
+        assertEquals("0",recognizeImage());
+    }
+
+    public String recognizeImage() {
+        Tesseract tesseract = new Tesseract();
+        tesseract.setTessVariable("user_defined_dpi", "270");
+        String actual = null;
+        try {
+            tesseract.setDatapath("/Users/bukalapak/Documents/qa-assessment-xendit");
+            String scanned = tesseract.doOCR(new File("result.png"));
+            actual = scanned.substring(0, scanned.length() - 1);
+        } catch (TesseractException e) {
+            e.printStackTrace();
+        }
+        return actual;
+    }
+
+    public void takeScreenShot() throws IOException {
         File screenshot = canvas.getScreenshotAs(OutputType.FILE);
         BufferedImage fullImg = ImageIO.read(screenshot);
 
         BufferedImage eleScreenshot= fullImg.getSubimage(25, 26, 354, 48);
         ImageIO.write(eleScreenshot, "png", screenshot);
 
-        File screenshotLocation = new File("/Users/bukalapak/Documents/qa-assessment-xendit/result.png");
+        File screenshotLocation = new File("result.png");
         FileHandler.copy(screenshot, screenshotLocation);
-
-        //OCR
-        Tesseract tesseract = new Tesseract();
-        tesseract.setTessVariable("user_defined_dpi", "270");
-        String actual = null;
-        try {
-            tesseract.setDatapath("/Users/bukalapak/Documents/qa-assessment-xendit");
-            String scanned = tesseract.doOCR(new File("/Users/bukalapak/Documents/qa-assessment-xendit/result.png"));
-            actual = scanned.substring(0, scanned.length() - 1);
-        } catch (TesseractException e) {
-            e.printStackTrace();
-        }
-        assertEquals(actual,expected);
-    }
-
-    @And("^user close the browser$")
-    public void userCloseTheBrowser() {
-        driver.close();
     }
 }
--- src/test/resource/calculator.feature
+++ src/test/resource/calculator.feature
@@ -1,12 +1,36 @@
+@calculator-functionality
 Feature: Test online calculator scenarios
+
+  @basic-operator
   Scenario Outline: Test subtraction, division and CE functionalities
     Given Open chrome browser and start application
-    When I enter following values and press enter button
+    When I enter following values for "basic" operation
       |value1 | <value1>|
       |value2 | <value2>|
       |operator | <operator>|
-    Then I should be able to see
+    And I press "ENTER" button
+    Then I should be able to see the right result
       |	expected |<expected>|
+    When I press "CE" button
+    Then The result should reset
+    And user close the browser
     Examples:
       | value1  		| value2 		| operator			| expected	|
-      | 	15 			|   15			|		+			| 30        |
+      | 	20 			|   20			|		+			| 40        |
+      | 	20 			|   20			|		-			| 0         |
+      | 	20 			|   20			|		*			| 400       |
+      | 	20 			|   20			|		/			| 1         |
+
+    @advance-operator
+    Scenario Outline: Test subtraction, division and CE functionalities
+      Given Open chrome browser and start application
+      When I enter following values for "advance" operation
+        |value1 | <value1>|
+        |button | <button>|
+      Then I should be able to see the right result
+        |	expected |<expected>|
+      And user close the browser
+      Examples:
+        | value1  		| button 		| expected			|
+        | 	10			|   %			|		0.1			|
+        | 	36 			|   √			|		6			|
